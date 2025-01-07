a21b2b850e8f7950f683412698b3b9dc0f519ff4
Rodney Madondo <rodney@finfind.co.za>
2025-01-07 11:36:02 +0200

    changed resume, added new projects in js, added project imgs, added new skills

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ function dynamicDate() {
 let projects = [
     {
         id:1,
-        img:"./images/signup.jpg",
-        title:"Newsletter Sign-up form",
-        desc:`Responsive HTML/CSS layout, designs obtained from frontendmentor.Utilizing HTML5 and CSS3 (SCSS) and Vanilla JavaScript for form validation and dynamic image source switching.`,
-        livelink:"https://rodneytakundamadondo.github.io/Newsletter-sign-up-form-with-success-message/",
-        github:"https://github.com/RodneyTakundaMadondo/Newsletter-sign-up-form-with-success-message.git"
+        img:"./images/space-tourism-website.png",
+        title:"Space Tourism Website",
+        desc:`An interactive space tourism site built with HTML, CSS, JavaScript, and TailwindCSS, featuring responsive design and smooth navigation`,
+        livelink:"https://rodneytakundamadondo.github.io/Space-tourism-website/",
+        github:"https://github.com/RodneyTakundaMadondo/Space-tourism-website.git"
     },
     {
         id:2,
-        img:"./images/fourfeature.jpg",
-        title:"Four-card-feature-section",
-        desc:`Responsive HTML/CSS layout, designs obtained from frontendmentor.Utilizing HTML5 and CSS3 (SCSS)`,
-        livelink:"https://rodneytakundamadondo.github.io/Four-card-feature-section/",
-        github:"https://github.com/RodneyTakundaMadondo/Four-card-feature-section.git"
+        img:"./images/multistepform.png",
+        title:"Multi Step Form Main",
+        desc:`A smooth, interactive multi-step form built with HTML, SCSS, and JavaScript, designed for improved user experience and responsive design.`,
+        livelink:"https://rodneytakundamadondo.github.io/Multi-step-form-main/",
+        github:"https://github.com/RodneyTakundaMadondo/Multi-step-form-main.git"
     },
+    
     {
         id:3,
         img:"./images/manage-landing.png",
@@ -34,6 +35,21 @@ let projects = [
         desc:`Responsive HTML/CSS layout, designs obtained from frontendmentor.Utilizing HTML5 and CSS3 (SCSS)`,
         livelink:"https://rodneytakundamadondo.github.io/E-commerce-product-page/",
         github:"https://github.com/RodneyTakundaMadondo/E-commerce-product-page.git"
+    },{
+        id:5,
+        img:"./images/signup.jpg",
+        title:"Newsletter Sign-up form",
+        desc:`Responsive HTML/CSS layout, designs obtained from frontendmentor.Utilizing HTML5 and CSS3 (SCSS) and Vanilla JavaScript for form validation and dynamic image source switching.`,
+        livelink:"https://rodneytakundamadondo.github.io/Newsletter-sign-up-form-with-success-message/",
+        github:"https://github.com/RodneyTakundaMadondo/Newsletter-sign-up-form-with-success-message.git"
+    },
+    {
+        id:6,
+        img:"./images/fourfeature.jpg",
+        title:"Four-card-feature-section",
+        desc:`Responsive HTML/CSS layout, designs obtained from frontendmentor.Utilizing HTML5 and CSS3 (SCSS)`,
+        livelink:"https://rodneytakundamadondo.github.io/Four-card-feature-section/",
+        github:"https://github.com/RodneyTakundaMadondo/Four-card-feature-section.git"
     }
 ]
 
